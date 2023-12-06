de2bd9c637e9a8edf6eb49a83aba8d78be2b69d4
maktum nagavi <maktumpatelsnagavi@gmail.com>
2023-12-06 13:51:52 +0530

    added custom details in swagger documentation

--- src/main/java/com/sample/swagger/helloWorld/HelloWorldApplication.java
+++ src/main/java/com/sample/swagger/helloWorld/HelloWorldApplication.java
@@ -1,9 +1,29 @@
 package com.sample.swagger.helloWorld;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Hello world",
+				version = "1.0",
+				description = "Sample Hello world Swagger",
+				termsOfService = "practice",
+				contact =@Contact(
+						name = "Maktum",
+						email = "m@gmail.com"
+						),
+				license = @License(
+						name = "licence",
+						url="runcodenow"
+				)
+		)
+)
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
